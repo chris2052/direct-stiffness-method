030d4ba06dfcd587d2c0761d4cfc5bd49af82e3a
Christopher Nies <chris-nies@hotmail.de>
2021-10-21 17:19:08 +0200

    added ge cases

--- load_f.m
+++ load_f.m
@@ -1,13 +1,30 @@
-function loadvec = load_f(a, b, f, l)
+function loadvec = load_f(a, b, f, l, ge)
 %LOAD Summary of this function goes here
 %   Detailed explanation goes here
 Ni = 0;
 Nk = 0;
-Mi = f * (a * b^2)/(l^2);
-Mk = -f * (a^2 * b)/(l^2);
-Vi = -f * (b^2)/(l^3) * (l + 2*a);
-Vk = -f * (a^2)/(l^3) * (l + 2*b);
 
+switch ge
+    case "ge1"
+        Mi = f * (a * b^2)/(l^2);
+        Mk = -f * (a^2 * b)/(l^2);
+        Vi = -f * (b^2)/(l^3) * (l + 2*a);
+        Vk = -f * (a^2)/(l^3) * (l + 2*b);
+        
+    case "ge2a"
+        Mi = f*b/2 * (1 - (b*b)/(l*l));
+        Mk = 0;
+        Vi = -f*b/(2*l) * (3 - (b*b)/(l*l));
+        Vk = -f/2 * (2 - 3*b/l + (b*b*b)/(l*l*l));
+        
+    case "ge2b"        
+        Mi = 0;
+        Mk = f*a/2 * (1 - (a*a)/(l*l));
+        Vi = -f/2 * (2 - 3*a/l + (a*a*a)/(l*l*l));
+        Vk = -f*a/(2*l) * (3 - (a*a)/(l*l));
+end
+                
 loadvec = [Ni, Vi, Mi, Nk, Vk, Mk];
+
 end
 
--- load_q.m
+++ load_q.m
@@ -1,13 +1,31 @@
-function loadvec = load_q(pi, pk, l)
+function loadvec = load_q(pi, pk, l, ge)
 %LOAD Summary of this function goes here
 %   Detailed explanation goes here
 Ni = 0;
 Nk = 0;
-Mi = l^2/60 * (3*pi + 2*pk);
-Mk = -l^2/60 * (2*pi + 3*pk);
-Vi = -l/60 * (21*pi + 9*pk);
-Vk = -l/60 * (9*pi + 21*pk);
+
+switch ge
+    case "ge1"
+        Mi = l^2/60 * (3*pi + 2*pk);
+        Mk = -l^2/60 * (2*pi + 3*pk);
+        Vi = -l/60 * (21*pi + 9*pk);
+        Vk = -l/60 * (9*pi + 21*pk);
+        
+    case "ge2a"
+        Mi = l*l/120 * (8*pi + 7*pk);
+        Mk = 0;
+        Vi = -l/120 * (48*pi + 27*pk);
+        Vk = -l/120 * (12*pi + 33*pk);
+        
+    case "ge2b"
+        Mi = 0;
+        Mk = l*l/120 * (7*pi + 8*pk);
+        Vi = -l/120 * (33*pi + 12*pk);
+        Vk = -l/120 * (27*pi + 48*pk);
+        
+end
 
 loadvec = [Ni, Vi, Mi, Nk, Vk, Mk];
+
 end
 
